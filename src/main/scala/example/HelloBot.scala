0e15cbb21f8ad526ddcb9fb91654f13eb3684f44
pme123 <feensturm@drachensegler.ch>
2018-02-08 09:15:33 +0100

    added HelloBot

---
+++ src/main/scala/example/HelloBot.scala
@@ -0,0 +1,24 @@
+package example
+
+import info.mukel.telegrambot4s.api.declarative.Commands
+import info.mukel.telegrambot4s.api.{Polling, TelegramBot}
+
+object HelloBot
+  extends TelegramBot // the general bot behavior
+    with Polling // we use Polling
+    with Commands { // and we want to listen to Commands
+
+  lazy val token: String = botToken // the token is required by the Bot behavior
+
+  onCommand('hello) { implicit msg => // listen for the command hello and
+    reply(s"Hello ${msg.from.map(_.firstName).getOrElse("")}!") // and reply with the personalized greeting
+  }
+}
+
+
+object BotApp extends App {
+  HelloBot.run()
+}
+
+
+
